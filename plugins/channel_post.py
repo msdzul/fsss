c61e4286f6df0ab3431987a8cc06ffc27a860afa
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-02-04 23:34:15 +0700

    🄼🅂 𝗗🆉𝗨𝗟𝚀𝐔𝐑𝐍Λ𝐈𝐍

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -17,7 +17,7 @@ from helper_func import encode
     filters.private
     & filters.user(ADMINS)
     & ~filters.command(
-        ["start", "pengguna", "siarkan", "ping", "aktif", "posts", "buatlink"]
+        ["start", "users", "broadcast", "ping", "uptime", "batch", "genlink"]
     )
 )
 async def channel_post(client: Client, message: Message):
