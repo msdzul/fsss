78abea195509ca0c6855e5809a459294efb31cb7
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-02-04 18:39:21 +0700

    Create ztext.py

---
+++ MSDZULQURNAIN/ztext.py
@@ -0,0 +1,9 @@
+# (©) @MSDZULQURNAIN
+# t.me/MSPR0JECT & t.me/MsSUPP0RT
+
+
+#START CONFIG
+zstart = "<b>Hallo {first}</b>\n\n<b>Saya adalah bot file sharing saya bisa mengirim kamu file melalui link yg telah dibuat\n\nSebelum menggunakan saya kamu wajib join channel saya.</b>"
+zfsub = "<b>Hallo {first}\n\nKamu harus join channel saya sebelum memakai saya\n\nJoin channel dibawah😉</b>"
+
+
