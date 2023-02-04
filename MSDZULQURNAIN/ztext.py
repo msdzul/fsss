d3e27be5acfdcd3ac8aa5d95314ef0cbc9a6697c
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-02-04 23:29:46 +0700

    🄼🅂 𝗗🆉𝗨𝗟𝚀𝐔𝐑𝐍Λ𝐈𝐍

--- MSDZULQURNAIN/ztext.py
+++ MSDZULQURNAIN/ztext.py
@@ -2,6 +2,10 @@
 # t.me/MSPR0JECT & t.me/MsSUPP0RT
 
 
+#INFO
+zinfo = "<b>❖BOT FILE SHARING TELEGRAM❖</b>\n\n<b>✰ Owner Repo : <a href='https://t.me/MSDZULQURNAIN'>🄼🅂 𝗗🆉𝗨𝗟𝚀𝐔𝐑𝐍Λ𝐈𝐍</a>\n⇝ Channel : <a href='https://t.me/MSPR0JECT'>🄼🅂 ק𝙍♢JΞC†</a>\n⇝ Support : <a href='https://t.me/MsSUPP0RT'>🄼🅂 Ꮥᴜקק♢ꭈׁׅ†</a>\n\nWAKTU ADALAH UANG BIJAKLAH MENGAMBIL LANGKAH😉</b>"
+
+
 #START CONFIG
 zstart = "<b>Hallo {first}</b>\n\n<b>Saya adalah bot file sharing saya bisa mengirim kamu file melalui link yg telah dibuat\n\nSebelum menggunakan saya kamu wajib join channel saya.</b>"
 zfsub = "<b>Hallo {first}\n\nKamu harus join channel saya sebelum memakai saya\n\nJoin channel dibawah😉</b>"
