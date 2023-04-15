6d60e269e258c07ea377e60e769180b4a5df5aae
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-04-15 23:55:03 +0700

    Update ztext.py

--- MSDZULQURNAIN/ztext.py
+++ MSDZULQURNAIN/ztext.py
@@ -6,8 +6,8 @@
 zinfo = "<b>❖BOT FILE SHARING TELEGRAM❖</b>\n\n<b>✰ Owner Repo : <a href='https://t.me/MSDZULQRNN'>🄼🅂 𝗗🆉𝗨𝗟𝚀𝐔𝐑𝐍Λ𝐈𝐍</a>\n⇝ Channel : <a href='https://t.me/MSPR0JECT'>🄼🅂 ק𝙍♢JΞC†</a>\n⇝ Support : <a href='https://t.me/MsSUPP0RT'>🄼🅂 Ꮥᴜקק♢ꭈׁׅ†</a>\n\nWAKTU ADALAH UANG BIJAKLAH MENGAMBIL LANGKAH😉</b>"
 
 #START CONFIG
-zstart = "<b>Hallo {first}</b>\n\n<b>Saya adalah bot file sharing saya bisa mengirim kamu file melalui link yg telah dibuat\n\nSebelum menggunakan saya kamu wajib join channel saya.</b>"
-zfsub = "<b>Hallo {first}\n\nKamu harus join channel saya sebelum memakai saya\n\nJoin channel dibawah😉</b>"
+zstart = "<b>Hallo {first}</b>\n\n<b>Saya adalah bot file sharing saya bisa mengirim kamu file melalui link yg telah dibuat\n\nSebelum menggunakan saya kamu wajib join channel saya\n\nMau bikin bot kaya gini? Pc @MSDZULQRNN</b>"
+zfsub = "<b>Hallo {first}\n\nKamu harus join channel saya sebelum memakai saya\n\nJoin channel dibawah😉\n\nMau bikin bot kaya gini? Pc @MSDZULQRNN</b>"
 
 #HEROKU
 zheroku = "Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda benar di config vars heroku"
