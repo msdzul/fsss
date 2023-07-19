4b9bb5d0394a883c1ac2a1e43cbe1ee71774062a
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-07-19 16:20:04 +0700

    Add files via upload

---
+++ plugins/button.py
@@ -0,0 +1,281 @@
+# (©) @mrismanaziz
+# t.me/SharingUserbot & t.me/Lunatic0de
+# Recode new by @MSDZULQURNAIN
+# t.me/MSPR0JECT & t.me/MsSUPP0RT
+
+from config import FSUB1, FSUB2, FSUB3, FSUB4, FSUB5, FSUB6, FSUB7, FSUB8, FSUB9, FSUB10
+
+from pyrogram.types import InlineKeyboardButton
+
+
+
+def fsub_button(client, message):
+    if FSUB1 and not FSUB2 and not FSUB3 and not FSUB4 and not FSUB5 and not FSUB6 and not FSUB7 and not FSUB8 and not FSUB9 and not FSUB10:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FSUB1 and FSUB2 and not FSUB3 and not FSUB4 and not FSUB5 and not FSUB6 and not FSUB7 and not FSUB8 and not FSUB9 and not FSUB10:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink2),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FSUB1 and FSUB2 and FSUB3 and not FSUB4 and not FSUB5 and not FSUB6 and not FSUB7 and not FSUB8 and not FSUB9 and not FSUB10:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink2),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink3),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FSUB1 and FSUB2 and FSUB3 and FSUB4 and not FSUB5 and not FSUB6 and not FSUB7 and not FSUB8 and not FSUB9 and not FSUB10:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink2),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink3),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink4),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+             pass
+        return buttons
+    if FSUB1 and FSUB2 and FSUB3 and FSUB4 and FSUB5 and not FSUB6 and not FSUB7 and not FSUB8 and not FSUB9 and not FSUB10:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink2),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink3),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink4),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink5),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FSUB1 and FSUB2 and FSUB3 and FSUB4 and FSUB5 and FSUB6 and not FSUB7 and not FSUB8 and not FSUB9 and not FSUB10:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink2),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink3),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink4),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink5),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink6),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FSUB1 and FSUB2 and FSUB3 and FSUB4 and FSUB5 and FSUB6 and FSUB7 and not FSUB8 and not FSUB9 and not FSUB10:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink2),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink3),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink4),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink5),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink6),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink7),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FSUB1 and FSUB2 and FSUB3 and FSUB4 and FSUB5 and FSUB6 and FSUB7 and FSUB8 and not FSUB9 and not FSUB10:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink2),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink3),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink4),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink5),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink6),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink7),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink8),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FSUB1 and FSUB2 and FSUB3 and FSUB4 and FSUB5 and FSUB6 and FSUB7 and FSUB8 and FSUB9 and not FSUB10:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink2),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink3),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink4),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink5),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink6),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink7),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink8),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink9),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FSUB1 and FSUB2 and FSUB3 and FSUB4 and FSUB5 and FSUB6 and FSUB7 and FSUB8 and FSUB9 and FSUB10:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink2),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink3),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink4),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink5),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink6),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink7),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink8),
+            ],
+            [
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink9),
+                InlineKeyboardButton(text="Join Disini", url=client.invitelink10),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Coba lagi",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
